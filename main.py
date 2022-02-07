c05be180ccaf9dbc13ee5f9b2efaee70284ff293
G1 <jeevanvaishnavv@gamil.com>
2022-02-08 02:33:19 +0530

    Final Commit

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.core.window import Window
-from kivymd.uix.bottomsheet import MDListBottomSheet, MDGridBottomSheet
+# from kivymd.uix.bottomsheet import MDListBottomSheet, MDGridBottomSheet
 from kivymd.toast import toast
+from kivymd.uix.bottomsheet import MDCustomBottomSheet
+from kivy.factory import Factory
+import webbrowser
 Window.size = (400, 600)
 
 
@@ -28,37 +31,50 @@ class BottomSheet(MDApp):
         self.theme_cls.primary_palette = "Lime"
         return screen
 
-    def callback_(self, *args):
-        toast(args[0])
+    def ShowBottomSheet(self):
+        self.custom = MDCustomBottomSheet(screen=Factory.CustomSheet())
+        self.custom.open()
 
-    def buttom_layer(self):
-        data = {
-            "Facebook": 'facebook-box',
-            "YouTube": 'youtube',
-            "Instagram": 'instagram',
-            "Twitter": 'twitter-box',
-            "Camera": 'camera'
-        }
-        bottom_sheet = MDGridBottomSheet(radius=20, radius_from="top")
-        for item in data.items():
-            bottom_sheet.add_item(
-                item[0],
-                lambda x, y=item[0]: self.callback_(y),
-                icon_src=item[1]
+    def open(self):
+        webbrowser.open_new("www.google.com")
 
-            )
-        bottom_sheet.open()
+        #
+        #
+        #
+        ##
+        #
+        #
+        # Second
+        # def callback_(self, *args):
+        #     toast(args[0])
+        # def buttom_layer(self):
+        #     data = {
+        #         "Facebook": 'facebook-box',
+        #         "YouTube": 'youtube',
+        #         "Instagram": 'instagram',
+        #         "Twitter": 'twitter-box',
+        #         "Camera": 'camera'
+        #     }
+        #     bottom_sheet = MDGridBottomSheet(radius=20, radius_from="top")
+        #     for item in data.items():
+        #         bottom_sheet.add_item(
+        #             item[0],
+        #             lambda x, y=item[0]: self.callback_(y),
+        #             icon_src=item[1]
+        #         )
+        #     bottom_sheet.open()
 
-    # def callback_(self, *args):
-    #     toast(args[0])
-    # def buttom_layer(self):
-    #     bottom_sheet = MDListBottomSheet(radius=20, radius_from='top')
-    #     for i in range(10):
-    #         bottom_sheet.add_item(
-    #             f"Item {i+1}",
-    #             lambda x, y=i+1: self.callback_(f"Item {y} Clicked"),
-    #             icon='android'
-    #         )
-    #     bottom_sheet.open()
+        # First
+        # def callback_(self, *args):
+        #     toast(args[0])
+        # def buttom_layer(self):
+        #     bottom_sheet = MDListBottomSheet(radius=20, radius_from='top')
+        #     for i in range(10):
+        #         bottom_sheet.add_item(
+        #             f"Item {i+1}",
+        #             lambda x, y=i+1: self.callback_(f"Item {y} Clicked"),
+        #             icon='android'
+        #         )
+        #     bottom_sheet.open()
 if __name__ == "__main__":
     BottomSheet().run()
